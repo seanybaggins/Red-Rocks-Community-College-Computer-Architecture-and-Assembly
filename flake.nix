409397a0cc0d019f89ea37180be9d0f0c0173523
Sean Link <seanybaggins1@gmail.com>
2026-05-29 15:41:46 -0600

    fix: attempting to build vm with garnix

--- flake.nix
+++ flake.nix
@@ -2,15 +2,34 @@
   description = "Top level flake for Red Rocks Community College computer architecture and assembly course";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs =
-    { self, nixpkgs }:
+    { self, nixpkgs }@inputs:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
     {
+
+      nixosConfigurations.rrcc-comp-arch-and-assembly-vm = inputs.nixpkgs.lib.nixosSystem {
+        inherit pkgs;
+        system = "x86_64-linux";
+        modules = [
+          "${inputs.nixpkgs}/nixos/modules/virtualisation/virtualbox-image.nix"
+          ./nixos/configuration.nix
+        ];
+
+        specialArgs = { inherit inputs; };
+      };
+
+      packages.x86_64-linux = {
+        default =
+          inputs.self.nixosConfigurations.rrcc-comp-arch-and-assembly-vm.config.system.build.virtualBoxOVA;
+      };
+
+      checks.x86_64-linux = inputs.self.packages.x86_64-linux;
+
       devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           mdbook
---
+++ nixos/configuration.nix
@@ -0,0 +1,95 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  imports = [
+  ];
+
+  # Boot configuration - use GRUB for VirtualBox compatibility
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+
+  # Enable emulation for ARM v7 binaries
+  boot.binfmt.emulatedSystems = [ "armv7l-linux" ];
+
+  # Enable VirtualBox guest additions
+  virtualisation.virtualbox.guest.enable = true;
+
+  # Configure disk size for VirtualBox OVA
+  virtualisation.diskSize = 10240; # 10GB disk
+
+  # Networking
+  networking.hostName = "student-vm";
+  networking.networkmanager.enable = true;
+
+  # Time zone
+  time.timeZone = "America/Denver";
+
+  # Internationalization
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Desktop environment
+  services.xserver = {
+    enable = true;
+    displayManager.lightdm.enable = true;
+    desktopManager.xfce.enable = true;
+    # VirtualBox graphics driver
+    videoDrivers = [
+      "vmware"
+      "vesa"
+      "modesetting"
+    ];
+  };
+
+  # User configuration
+  users.users.student = {
+    isNormalUser = true;
+    description = "Student User";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "vboxsf"
+      "dialout"
+      "plugdev"
+    ];
+    initialPassword = "student";
+  };
+
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = "student";
+  };
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    #xfce.xfce4-terminal
+    gcc
+    pkgsCross.armv7l-hf-multiplatform.buildPackages.gcc
+  ];
+
+  # Enable SSH for remote access
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = true;
+      PermitRootLogin = "no";
+    };
+  };
+
+  # Firewall
+  #networking.firewall.enable = true;
+  #networking.firewall.allowedTCPPorts = [ 22 ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  system.stateVersion = "25.11";
+
+}
